600cc804bc3ee478d83a8b187eb97ce93f926cbc
Tycjan Falana <tycjan0605@gmail.com>
2023-04-27 17:18:42 +0200

    Added protection against non-admin users.

--- frontend/src/pages/acp.tsx
+++ frontend/src/pages/acp.tsx
@@ -1,17 +1,54 @@
 import Head from "next/head";
 import Layout from "@/Components/Layout";
-import Header from "@/Components/Header";
-import styles from "@/Styles/Content.module.scss"
 import acpStyles from "@/Styles/Pages/acp.module.scss"
 import ButtonStyles from "@/Styles/New/Buttons.module.scss"
 import List from "@/Components/List";
-import {Button} from "react-bootstrap";
 import AddDeviceModal from "@/Components/ACP/AddDeviceModal";
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {useSession} from "@/Context/SessionProvider";
+import {checkToken} from "@/Helpers/Token";
+import {useRouter} from "next/router";
+import {useAlert} from "@/Context/AlertProvider";
+import axios from "axios";
+import {routes} from "@/Config";
 
 export default function AdminControlPanel(){
-    let columnHead = [{key: "name", text: "Name & Company", sortable: true}, {key: "date", text:"Date", sortable: true}, {key: "availability", text: "Availability", sortable: true}, {key: "acpActions", text: "Actions"}];
-    let [showModal, setShowModal] = useState(false);
+    const {sessionData} = useSession();
+    const {editAlert} = useAlert();
+    const columnHead = [{key: "name", text: "Name & Company", sortable: true}, {key: "date", text:"Date", sortable: true}, {key: "availability", text: "Availability", sortable: true}, {key: "acpActions", text: "Actions"}];
+    const [showModal, setShowModal] = useState(false);
+    const {push} = useRouter();
+
+    useEffect(()=>{
+        let token = localStorage.getItem("token");
+        checkToken(token!).then(valid=>{
+            if(!valid){
+                localStorage.removeItem("token");
+                push("/auth")
+                return
+            }
+            if(!sessionData.isAdmin){
+                editAlert(true, "warning", "Invalid permissions.");
+                push("/")
+                return
+            }
+            axios.get(routes.currentUserInfo, {
+                "headers": {"x-access-token": token}
+            }).then(resp=>{
+                let {admin}: {admin: boolean} = resp.data.data;
+                if(!admin){
+                    editAlert(true, "warning", "Invalid permissions.");
+                    push("/")
+                }
+            }).catch(e=>{
+                localStorage.removeItem("token");
+                push("/auth")
+            })
+        }).catch(e=>{
+            localStorage.removeItem("token");
+            push("/auth")
+        })
+    }, [])
 
     let handleShow = ()=>{
         setShowModal(true);
@@ -29,16 +66,19 @@ export default function AdminControlPanel(){
 
     return(
         <>
-            <Head>
-                <title>Admin Control Panel - Hardware Rental</title>
-            </Head>
-            <Layout HeaderText={"Admin Control Panel"}>
-                <div className={acpStyles.controlBox}>
-                    <button className={`${ButtonStyles.button} ${ButtonStyles.green}`} onClick={handleShow}>➕ Add device</button>
-                </div>
-                <List columnHeadData={columnHead} buttonClickHandler={[handleEdit, handleSendToRepair, handleRemove]}/>
-                <AddDeviceModal showModal={showModal} setShowModal={setShowModal}/>
-            </Layout>
+            {!sessionData.isAdmin|| <>
+                <Head>
+                    <title>Admin Control Panel - Hardware Rental</title>
+                </Head>
+                <Layout HeaderText={"Admin Control Panel"}>
+                    <div className={acpStyles.controlBox}>
+                        <button className={`${ButtonStyles.button} ${ButtonStyles.green}`} onClick={handleShow}>➕ Add device</button>
+                    </div>
+                    <List columnHeadData={columnHead} buttonClickHandler={[handleEdit, handleSendToRepair, handleRemove]}/>
+                    <AddDeviceModal showModal={showModal} setShowModal={setShowModal}/>
+                </Layout>
+            </>}
+
         </>
     )
 }
