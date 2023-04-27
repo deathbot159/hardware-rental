f702fa2adadf8505802645a7f665bfd24d47a89d
Tycjan Falana <tycjan0605@gmail.com>
2023-04-27 18:41:32 +0200

    Prepared AddDeviceModal.tsx for handle of adding devices.

--- frontend/src/Components/ACP/AddDeviceModal.tsx
+++ frontend/src/Components/ACP/AddDeviceModal.tsx
@@ -1,14 +1,42 @@
 import {Button, Form, InputGroup, Modal} from "react-bootstrap";
+import {useEffect, useState} from "react";
+import {useAlert} from "@/Context/AlertProvider";
 
+type newDeviceData = {name: string, company: string, disabled: boolean, tryingToAdd: boolean}
 
 export default function AddDeviceModal({showModal, setShowModal}:{showModal: boolean, setShowModal: any}){
+    const [newDeviceData, setNewDeviceData] = useState({name: "", company: "", disabled: false, tryingToAdd: false} as newDeviceData)
+    const {editAlert} = useAlert();
 
-    let handleClose = ()=>{
-        setShowModal(false);
+    const handleClose = ()=>{
+        if(!newDeviceData.tryingToAdd) {
+            setNewDeviceData({name: "", company: "", disabled: false, tryingToAdd: false});
+            setShowModal(false);
+        }
     }
 
-    let handleAdd = () => {
-        setShowModal(false);
+    const toggleButtons = () => {
+        setNewDeviceData(prev=>({...prev, tryingToAdd: !prev.tryingToAdd}));
+    }
+
+    const handleAdd = () => {
+        if(!newDeviceData.tryingToAdd){
+            toggleButtons();
+            let {name, company, disabled} = newDeviceData;
+            if(name == "" || company == ""){
+                editAlert(true, "warning", "Please, provide name and company of device.");
+                toggleButtons()
+                return;
+            }
+
+            handleClose();
+        }
+    }
+
+    const handleInput = (key: "name"|"company"|"disabled", value: string|boolean) => {
+        let obj: any = {}
+        obj[key] = value
+        setNewDeviceData(prev=>({...prev, ...obj}))
     }
 
     return <Modal
@@ -22,28 +50,35 @@ export default function AddDeviceModal({showModal, setShowModal}:{showModal: boo
             </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-            <InputGroup className="mb-3">
-                <InputGroup.Text>Name & Company</InputGroup.Text>
+            <InputGroup className={"mb-3"}>
+                <InputGroup.Text>Company</InputGroup.Text>
+                <Form.Control
+                    placeholder="Company name"
+                    aria-label="Company name"
+                    onChange={ev=>handleInput("company", ev.target.value)}
+                    required
+                />
+            </InputGroup>
+            <InputGroup className={"mb-3"}>
+                <InputGroup.Text>Name</InputGroup.Text>
                 <Form.Control
                     placeholder="Device name"
                     aria-label="Device name"
+                    onChange={ev=>handleInput("name", ev.target.value)}
+                    required
                 />
             </InputGroup>
-            <InputGroup className="mb-3">
-                <InputGroup.Text>Date</InputGroup.Text>
-                <Form.Control
-                    type={"date"}
-                    placeholder="deviceAddDate"
+            <InputGroup className={"mb-3"}>
+                <InputGroup.Text>Disabled</InputGroup.Text>
+                <InputGroup.Checkbox
+                    aria-label="Checkbox disabled"
+                    onChange={(ev: any)=>handleInput("disabled", !newDeviceData.disabled)}
                 />
-            </InputGroup>
-            <InputGroup className="mb-3">
-                <InputGroup.Checkbox aria-label="Checkbox disabled" />
-                <Form.Control readOnly placeholder={"Disable device"} aria-label="Disable device" />
             </InputGroup>
         </Modal.Body>
         <Modal.Footer>
-            <Button onClick={handleAdd} variant={"success"}>Add</Button>
-            <Button onClick={handleClose} variant={"danger"}>Close</Button>
+            <Button onClick={handleAdd} variant={"success"} disabled={newDeviceData.tryingToAdd}>Add</Button>
+            <Button onClick={handleClose} variant={"danger"} disabled={newDeviceData.tryingToAdd}>Close</Button>
         </Modal.Footer>
     </Modal>
 }
