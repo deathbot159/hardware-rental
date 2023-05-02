759d678cc0686e0cd69bff2e785f6d3f1ac7552b
Tycjan Falana <tycjan0605@gmail.com>
2023-05-02 20:27:59 +0200

    Removed unused editAlert reference. Added checking of nullified token in refreshData.

--- frontend/src/Context/DeviceDataProvider.tsx
+++ frontend/src/Context/DeviceDataProvider.tsx
@@ -1,7 +1,6 @@
 import {createContext, ReactNode, useContext, useEffect, useState} from "react";
 import {DeviceDataType, IDevice} from "@/@Types/DeviceDataTypes";
 import {useSession} from "@/Context/SessionProvider";
-import {useAlert} from "@/Context/AlertProvider";
 import {useRouter} from "next/router";
 import {DeviceState} from "@/Helpers/DeviceState";
 import API from "@/Helpers/API";
@@ -10,7 +9,6 @@ export const DeviceDataContext = createContext<DeviceDataType | null>(null);
 
 export default function DeviceDataProvider({children}: {children: ReactNode}){
     const {sessionData} = useSession();
-    const {editAlert} = useAlert();
     const [devices, setDevicesState] = useState([] as IDevice[]);
     const [loadingDevices, setLoadingDevices] = useState(true);
     const {push} = useRouter();
@@ -23,23 +21,23 @@ export default function DeviceDataProvider({children}: {children: ReactNode}){
 
     const refreshData = async () => {
         let token = localStorage.getItem("token");
-        if (token != null) {
-            let {success, data} = await API.getDevices();
-            if(success){
-                setDevicesState(
-                    data!.map(d => ({
-                        ...d,
-                        name: `${d.company} ${d.name}`,
-                        availability: !sessionData.isAdmin ? d.state != 0 ? DeviceState.NotAvilable : DeviceState._ : d.state
-                    }))
-                        .sort((a, b) => (a.name < b.name) ? -1 : 1)
-                );
-                setLoadingDevices(false);
-            }else{
-                localStorage.removeItem("token");
-                push("/auth");
-            }
+        if(token == null) return;
+        let {success, data} = await API.getDevices();
+        if(success){
+            setDevicesState(
+                data!.map(d => ({
+                    ...d,
+                    name: `${d.company} ${d.name}`,
+                    availability: !sessionData.isAdmin ? d.state != 0 ? DeviceState.NotAvilable : DeviceState._ : d.state
+                }))
+                    .sort((a, b) => (a.name < b.name) ? -1 : 1)
+            );
+            setLoadingDevices(false);
+        }else{
+            localStorage.removeItem("token");
+            await push("/auth");
         }
+
     }
 
 
